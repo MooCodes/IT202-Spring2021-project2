01bbab43dff8b3847ea210824c8a695bbbf4c561
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-03-27 12:34:59 -0500

    refd the canvas and drew a simple circle

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('testing')
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+// set the canvas width/height
+canvas.width = innerWidth / 2
+canvas.height = innerHeight / 2
+
+// draw a circle
+c.beginPath()
+c.arc(50, 50, 10, Math.PI * 2, false)
+c.fillStyle = 'blue'
+c.fill()
