5873ad1dd2dbf11686c82c171ae8892cb5447b12
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-03-27 14:24:02 -0500

    Added a way to load in images into the game

--- index.js
+++ index.js
@@ -5,8 +5,35 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth / 2
 canvas.height = innerHeight / 2
 
-// draw a circle
-c.beginPath()
-c.arc(50, 50, 10, Math.PI * 2, false)
-c.fillStyle = 'blue'
-c.fill()
+console.log(canvas.width, canvas.height)
+
+// Define & load in our game images
+const playerImg = new Image()
+playerImg.src = './img/player.png'
+const bgImg = new Image()
+bgImg.src = './img/background.jpg'
+
+const imageUrls = ['player.png', 'background.jpg']
+const images = []
+let imgCount = 0
+
+const playGame = () => {
+    // draw background and then foreground
+    c.drawImage(images[1], 0, 0, canvas.width, canvas.height)
+    c.drawImage(images[0], 0, 0)
+}
+
+imageUrls.forEach(src => {
+    const image = new Image()
+    image.src = `./img/${src}`
+    image.onload = () => {
+        imgCount++
+        if (imgCount == imageUrls.length) {
+            // all images have been loaded
+            // start the game
+            playGame()
+        }
+    }
+    images.push(image)
+})
+
